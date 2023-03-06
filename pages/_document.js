bbc47d91ae8eac6c2fa4e0cec267510729dc6ff1
kat <katrinashui1992@gmail.com>
2023-03-06 16:10:58 +0800

    Add existing project files to Git

--- pages/_document.js
+++ pages/_document.js
@@ -3,6 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
+        <link href="https://fonts.googleapis.com/css?family=Space+Mono:400,700" rel="stylesheet" />
       <Head />
       <body>
         <Main />
